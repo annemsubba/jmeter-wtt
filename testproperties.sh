19967cf126f32f9491d0de0e0404436f43f267d5
annemsubba <84909464+annemsubba@users.noreply.github.com>
2025-05-06 18:01:32 +0530

    Update testproperties.sh

--- testproperties.sh
+++ testproperties.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-instanceFile="${WORKSPACE}/test.properties"
+instanceFile=${WORKSPACE}/test.properties
 if [ -f "$instanceFile" ]; then
-   # Extract just the values
-   applicationurl=$(grep '^url=' "$instanceFile" | cut -d '=' -f2-)
-   indexname=$(grep '^index=' "$instanceFile" | cut -d '=' -f2-)
-   esurl=$(grep '^host=' "$instanceFile" | cut -d '=' -f2-)
-   testplanId=$(grep '^runid=' "$instanceFile" | cut -d '=' -f2-)
+   application_url=$(cat "$instanceFile" | grep url)
+   index_name=$(cat "$instanceFile" | grep index)
+   es_url=$(cat "$instanceFile" | grep host)
+   test_planId=$(cat "$instanceFile" | grep runid)
 
-   # Construct updated lines
+
+   
    updated_application_url="url=$applicationurl"
    updated_index_name="index=$indexname"
    updated_es_url="host=$esurl"
    updated_test_planId="runid=$testplanId"
-
-   # SED to replace lines
-   sed -i "s|^url=.*|$updated_application_url|" "$instanceFile"
-   sed -i "s|^index=.*|$updated_index_name|" "$instanceFile"
-   sed -i "s|^host=.*|$updated_es_url|" "$instanceFile"
-   sed -i "s|^runid=.*|$updated_test_planId|" "$instanceFile"
+    
+   # SED command to replace exisitng line with newly updated line in InstanceFile
+   sed -i "s|$application_url|$updated_application_url|g" "$instanceFile"
+   sed -i "s|$index_name|$updated_index_name|g" "$instanceFile"
+   sed -i "s|$es_url|$updated_es_url|g" "$instanceFile"
+   sed -i "s/$test_planId/$updated_test_planId/g" "$instanceFile"
 fi
